e2bfbe240deee588b3f7b8433b7a789929bd17c2
tlemme <thomas.lemme@rise-world.com>
2012-08-25 13:27:06 +0200

    jetzt funkts

--- springtest/src/main/java/at/lemme/App.java
+++ springtest/src/main/java/at/lemme/App.java
@@ -1,13 +1,30 @@
 package at.lemme;
 
+import javax.faces.bean.ManagedBean;
+
 /**
  * Hello world!
  *
  */
+@ManagedBean(name="helloBean")
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    private String value;  
+    
+    public String getValue() {  
+        return value;  
+    }  
+  
+    public void setValue(String value) {  
+        this.value = value;  
+    }  
+    
+    public String getHello(){
+    	return "hello 123";
     }
 }
